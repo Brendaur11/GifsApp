20b4f8840e9e9fae0d7638778997673109080c76
Brenda Urquiza <urquizabrenda11@gmail.com>
2025-05-21 21:13:47 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import {GifExpertApp} from './pages/GifExpertApp';
+import { GifExpertApp } from './pages/GifExpertApp';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/GifsApp">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/busqueda" element={<GifExpertApp />} />
